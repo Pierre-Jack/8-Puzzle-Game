6b67d5ca29b51caeb276b8aadb44c77444d94f2a
Armia Joseph <armia.joseph35@gmail.com>
2024-10-22 14:03:16 +0300

    adding html file

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from collections import deque
 def get_children(state):  #missing number is 0
      children = []
      s = str(state)
-     if(len(s) < 9): s = "0" + s
+     if len(s) < 9: s = "0" + s
      i = s.find("0")
 
      for j in [-3, -1, 1, 3]:
           ns = list(s)
           
-          if(0 <= i+j and i+j < 9):
+          if 0 <= i+j < 9:
             if not (j == -1 and i%3 == 0):
                if not (j == 1 and i%3 == 2):
                 tmp = ns[i]
@@ -26,7 +26,7 @@ def solve_bfs(initial):   #missing number is 0 #
     state = initial
 
     d[state] = -1
-    if(state == goal):
+    if state == goal:
          return True, d
     q.append(state)
     
@@ -51,12 +51,12 @@ def get_path(parent_dict):  #gets the path from the dict of states -- (node, par
 def get_direction(state1, state2):
         val = {-3: "UP", -1: "LEFT", 1: "RIGHT", 3: "DOWN",}
         s = str(state1)
-        if(len(s) < 9): s = "0" + s
+        if len(s) < 9: s = "0" + s
         i = s.find("0")
 
         for j in [-3, -1, 1, 3]:
             ns = list(s)
-            if(0 <= i+j and i+j < 9):
+            if 0 <= i+j < 9:
                 if not (j == -1 and i%3 == 0):
                     if not (j == 1 and i%3 == 2):
                         tmp = ns[i]
@@ -69,23 +69,49 @@ def get_directions(path):
     directions = []
     for i in range(len(path)-1):
         directions.append(get_direction(path[i], path[i+1]))
-    return directions
-
-     
-     
-     
-
-
-         
+    return directions       
 
 init = 603247851
 
   
 
-solvable, map = solve_bfs(init)
-print (solvable) 
-if solvable:
-    print(get_directions(get_path(map)))
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        received_array = json.loads(post_data)
+        print(f'Received array: {received_array}')
+
+        # response array
+        response_array = []
+        init = int(''.join(map(str, received_array)))
+        solvable, m = solve_bfs(init)
+        if solvable:
+            response_array = get_directions(get_path(m))
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        response = {'array': response_array}
+        self.wfile.write(json.dumps(response).encode())
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
 
